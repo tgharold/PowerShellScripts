bf12b5d83c503986e54bff32cc064e63972c7479
Tom Harold <tom.harold@integrity.com>
2025-02-07 09:43:48 -0500

    Change to make the function pipe-aware

--- SortObjectProperties/SortObjectProperties.ps1
+++ SortObjectProperties/SortObjectProperties.ps1
@@ -2,23 +2,19 @@
 Param()
 
 function Sort-ObjectPropertiesByName {
-    param(
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        $InputObject
-    )
-
     process {
-        $typeList = $InputObject.GetType() | Select-Object -Property Name, FullName, BaseType
-        Write-Debug "`$InputObject{$($typeList)} = $($InputObject)"
+        if ($_ -eq $null) { return }
+        Write-Debug "`$_: $($_)"
+        $typeList = $_.GetType() | Select-Object -Property Name, FullName, BaseType
+        Write-Debug "`$_{$($typeList)} = $($_)"
 
-        if ($InputObject -is [System.Collections.IDictionary]) {
+        if ($_ -is [System.Collections.IDictionary]) {
             $sortedObject = [ordered]@{}
-            foreach($key in $InputObject.Keys | Sort-Object) {
-                $value = $InputObject[$key]
+            foreach($key in $_.Keys | Sort-Object) {
+                $value = $_[$key]
                 Write-Debug "key-value[$($key)]: $($value)"
                 if ($value -is [PSCustomObject] -or $value -is [System.Management.Automation.PSObject] -or $value -is [System.Collections.IDictionary]) {
-                    $sortedValue = Sort-ObjectPropertiesByName -InputObject $value
+                    $sortedValue = $value | Sort-ObjectPropertiesByName
                     $sortedObject.Add($key, $sortedValue)
                 } else {
                     $sortedObject.Add($key, $value)
@@ -27,17 +23,17 @@ function Sort-ObjectPropertiesByName {
             Write-Debug "`$sortedObject{$($sortedObject.GetType())} = $($sortedObject)"
             $sortedObject
         } else {
-            $properties = $InputObject.PSObject.Properties | Sort-Object Name
+            $properties = $_.PSObject.Properties | Sort-Object Name
             $sortedObject = [ordered]@{}
             foreach ($property in $properties) {
-                $typeList = $property.Value.GetType() | Select-Object -Property Name, FullName, BaseType
+                $typeList = $property.Value?.GetType() | Select-Object -Property Name, FullName, BaseType
                 Write-Debug "property{$($typeList)}: $($property.Value)"
                 if ($property.Value -is [PSCustomObject] -or $property.Value -is [System.Management.Automation.PSObject] -or $property.Value -is [System.Collections.IDictionary]) {
-                    $value = Sort-ObjectPropertiesByName -InputObject $property.Value
+                    $value = $property.Value | Sort-ObjectPropertiesByName
                     $sortedObject.Add($property.Name, $value)
                 } elseif ($property.Value -is [array]) {
                     Write-Debug "property-array{$($typeList)}: $($property.Value)"
-                    $sortedValues = $property.Value | Sort-Object -Stable
+                    $sortedValues = @($property.Value | Sort-Object -Stable)
                     $sortedObject.Add($property.Name, $sortedValues)
                 } else {
                     $sortedObject.Add($property.Name, $property.Value)
@@ -51,13 +47,17 @@ function Sort-ObjectPropertiesByName {
 
 $input1 = [PSCustomObject][ordered]@{
     Bell = 5
+    Zoo = $null
     Zulu = "b", "a", "t", "h", "f"
+    Tags = @("Yellow")
     Taco = 7
     Gerald = @{
         Zebra = "Pink"
         Nada = @{
+            T = $null
             Seven = "Alpha"
             Bravo = "Eight"
+            V = @(5)
             Q = [PSCustomObject][ordered]@{        
                 Charred = 5, 3, 2, 7, 9
             }
@@ -71,7 +71,7 @@ Write-Debug "`$input1.GetType() = $($input1.GetType()  | Select-Object -Property
 Write-Debug $($input1 | ConvertTo-Json -Depth 100)
 Write-Debug "..."
 
-$output1 = Sort-ObjectPropertiesByName -InputObject $input1
+$output1 = $input1 | Sort-ObjectPropertiesByName
 Write-Debug "`$output1.GetType() = $($output1.GetType()  | Select-Object -Property Name, FullName, BaseType)"
 Write-Debug $($output1 | ConvertTo-Json -Depth 100)
 Write-Debug "..."
